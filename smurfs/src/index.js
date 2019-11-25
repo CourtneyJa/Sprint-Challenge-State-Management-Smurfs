abc1f7da6b8cbbb31869d9ecaa64589c6d2a621b
Courtney Jackson <52590162+CoderZim@users.noreply.github.com>
2019-11-24 22:22:29 -0500

    dependencies all added, self study answered

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { reducer } from "./components/Reducers/Reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
